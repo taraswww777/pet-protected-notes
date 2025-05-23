ca630db55e251d0407e6edcb74a2a24514e3106c
Тарас Викторович (4067694) Ковалёв <kovalevtv@vtb.ru>
2025-05-23 14:56:57 +0300

    #18 add icons

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - [Запуск проекта](./doc/StartProject.md)
 - [Документация](./doc/README.md)
 - [Nestjs](./doc/FrontEnd)
+- Иконки - https://icon-sets.iconify.design
--- fe/tailwind.config.ts
+++ fe/tailwind.config.ts
@@ -1,24 +1,13 @@
 import type { Config } from 'tailwindcss'
-import { Icons, type Options } from 'tailwindcss-plugin-icons'
-
-const options: Options = ({ theme }) => ({
-  heroicons: {
-    icons: {
-      'plus-circle': {
-        cursor: 'pointer',
-        color: theme('colors.emerald.600'),
-        '&:hover': {
-          color: theme('colors.emerald.800'),
-        },
-      },
-      'trash?bg': {},
-    },
-    includeAll: true,
-    scale: iconName => (iconName.endsWith('-20-solid') ? 1.25 : 1.5),
-    location: 'https://esm.sh/@iconify-json/heroicons@1.1.9/icons.json',
-  },
-})
+import { addDynamicIconSelectors } from '@iconify/tailwind';
 
 export default {
-  plugins: [Icons(options)],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [addDynamicIconSelectors()],
 } as Config
