39097343db0b9e53b57af38009364f35e99eddf0
taraswww777 <taraswww777@mail.ru>
2025-08-14 17:17:01 +0300

    #91 add ActionsManagerPage

--- fe/src/modules/dashboard/DashboardRouter.tsx
+++ fe/src/modules/dashboard/DashboardRouter.tsx
@@ -1,22 +1,11 @@
 import { Route, Routes } from 'react-router';
 import { Suspense } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
-import { DashboardHomePage, RolesPage } from './pages';
+import { ActionsManagerPage, DashboardHomePage, RolesPage } from './pages';
 import { ErrorFallback } from '../../components/ErrorFallback.tsx';
 import { Loader } from '../../uiKit/Loader.tsx';
 import { DashboardLayout } from './components/DashboardLayout.tsx';
 
-
-// import { Route, Routes } from 'react-router';
-// import { Suspense } from 'react';
-// import { ErrorBoundary } from 'react-error-boundary';
-// import { DashboardLayout } from './components/DashboardLayout';
-// import { RolesPage } from './pages/RolesPage';
-// import { ActionsPage } from './pages/ActionsPage';
-// import { UsersPage } from './pages/UsersPage';
-// import { SettingsPage } from './pages/SettingsPage';
-// import { ErrorFallback } from '../../components/ErrorFallback';
-// import { Loader } from '../../uiKit/Loader';
 
 export const DashboardRouter = () => {
   return (
@@ -26,10 +15,9 @@ export const DashboardRouter = () => {
           <Routes>
             <Route index element={<DashboardHomePage />} />
             <Route path="roles" element={<RolesPage />} />
-            {/*<Route path="actions" element={<ActionsPage />} />*/}
+            <Route path="actions" element={<ActionsManagerPage />} />
             {/*<Route path="users" element={<UsersPage />} />*/}
             {/*<Route path="settings" element={<SettingsPage />} />*/}
-            {/*<Route index element={<RolesPage />} />*/}
           </Routes>
         </Suspense>
       </ErrorBoundary>
--- fe/src/modules/dashboard/pages/index.ts
+++ fe/src/modules/dashboard/pages/index.ts
@@ -2,3 +2,4 @@ import { lazy } from 'react';
 
 export const DashboardHomePage = lazy(() => import('./DashboardHomePage.tsx'));
 export const RolesPage = lazy(() => import('./RolesPage.tsx'));
+export const ActionsManagerPage = lazy(() => import('./ActionsManagerPage.tsx'));
