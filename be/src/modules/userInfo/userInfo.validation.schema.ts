9562cfc9ab91780ff8d3ea0b147677a45a196cd2
taraswww777 <taraswww777@mail.ru>
2025-08-28 18:56:40 +0300

    #53 split zod validations to common and be

--- be/src/modules/userInfo/userInfo.validation.schema.ts
+++ be/src/modules/userInfo/userInfo.validation.schema.ts
@@ -3,32 +3,11 @@ import { createInsertSchema, createSelectSchema, createUpdateSchema } from 'driz
 import { schema } from '../../db';
 import { z } from 'zod';
 import { userInfo } from '../../db/schemas';
-
-// общие атомарные схемы валидаций
-
-/** Функция для создания схемы имени с общими правилами валидации */
-const firstName = (schema: z.ZodString) =>
-  schema
-    .min(2, 'Имя должно содержать минимум 2 символа')
-    .max(50, 'Имя не должно превышать 50 символов')
-    .regex(/^[a-zA-Zа-яА-ЯёЁ\s-]+$/, 'Имя может содержать только буквы, дефисы и пробелы')
-    .nullable()
-    .optional();
-
-const secondName = (schema: z.ZodString) => schema
-  .min(2, 'Отчество должно содержать минимум 2 символа')
-  .max(50, 'Отчество не должно превышать 50 символов')
-  .regex(/^[a-zA-Zа-яА-ЯёЁ\s-]+$/, 'Отчество может содержать только буквы, дефисы и пробелы')
-  .nullable()
-  .optional();
-
-const thirdName = (schema: z.ZodString) => schema
-  .min(2, 'Фамилия должна содержать минимум 2 символа')
-  .max(50, 'Фамилия не должна превышать 50 символов')
-  .regex(/^[a-zA-Zа-яА-ЯёЁ\s-]+$/, 'Фамилия может содержать только буквы, дефисы и пробелы')
-  .nullable()
-  .optional();
-
+import {
+  firstNameValidations,
+  secondNameValidations,
+  thirdNameValidations
+} from 'protected-notes-common/src/zodValidators/fioValidations';
 
 
 // конкретные бизнес схемы для валидации
@@ -37,10 +16,10 @@ const thirdName = (schema: z.ZodString) => schema
 export const userInfoSelectSchema = createSelectSchema(schema.userInfo);
 export const userInfoInsertSchema = createInsertSchema(schema.userInfo);
 
-export const userInfoUpdateValidationSchema  = createUpdateSchema(schema.userInfo, {
-  firstName,
-  secondName,
-  thirdName
+export const userInfoUpdateValidationSchema = createUpdateSchema(schema.userInfo, {
+  firstName: firstNameValidations,
+  secondName: secondNameValidations,
+  thirdName: thirdNameValidations
 })
   .partial() // Все поля опциональны для обновления
   .omit({ userId: true }); // Исключаем userId из тела запроса
@@ -48,12 +27,11 @@ export const userInfoUpdateValidationSchema  = createUpdateSchema(schema.userInf
 
 // Схема для создания (обязательные поля)
 export const userInfoCreateValidationSchema = createInsertSchema(userInfo, {
-  firstName,
-  secondName,
-  thirdName
+  firstName: firstNameValidations,
+  secondName: secondNameValidations,
+  thirdName: thirdNameValidations
 })
   .omit({ userId: true }); // userId передается отдельно
-
 
 
 export type UserInfoSelect = z.infer<typeof userInfoSelectSchema>;
